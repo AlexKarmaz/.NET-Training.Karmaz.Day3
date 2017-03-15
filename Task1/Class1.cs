749f5a3629b2500f1e25c14ba3d8550f88356a39
AlexKarmaz <karmazct@mail.ru>
2017-03-16 00:02:52 +0300

    Add project files.

---
+++ Task1/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    /// <summary>
+    /// This class contains methods for working with arrays
+    /// </summary>
+    public static class Class1
+    {
+        #region Public methods
+        /// <summary>
+        /// Searches the smallest index in the array which the sum of the elements
+        /// to the left of it is equal to the sum of the elements on the right.
+        /// </summary>
+        /// <param name="array">The System.Array in which the index is searchedy</param>
+        /// <returns>The integer value of the array index for which the sum of the elements
+        /// to the left of it is equal to the sum of the elements on the right.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static int ReturnIndex(int[] array)
+        {
+            if(array == null)
+            {
+                throw new ArgumentNullException("array");
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (SearchIndex(i, array)) return i;
+            }
+            return -1;
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Checks for a specific index in an array the condition:the sum of the elements to the left
+        /// of it must be equal to the sum of the elements on the right.
+        /// </summary>
+        /// <param name="array">The System.Array in which the index is searched</param>
+        /// <param name="index"> The index in the array for which the condition is checked </param>
+        /// <returns>True if the array has an index for which the condition is satisfied.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        private static bool SearchIndex(int index, int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array");
+            }
+            if(index < 0)
+            {
+                throw new ArgumentException();
+            }
+            double sum1 = array.ArraySum(0, index);
+            double sum2 = array.ArraySum(index + 1, array.Length);
+
+            if (sum1 == sum2) return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Counts the sum of the elements of the array, starting and ending with the elements whose indices are given.
+        /// </summary>
+        /// <param name="data">The System.Array in which you want to find the sum of the elements</param>
+        /// <param name="startIndex">The index of the element from which the sum is considered</param>
+        /// <param name="endIndex">The index of the element to which the sum is considered</param>
+        /// <returns>The sum of array elements</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        private static double ArraySum( this int[] data, int startIndex, int endIndex)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException("data");
+            }
+            if (startIndex < 0 || endIndex < 0)
+            {
+                throw new ArgumentException();
+            }
+            double sum = 0;
+
+             for(int i = startIndex; i < endIndex; i++)
+             {
+                  sum += data[i];
+             }
+          
+            return sum;
+        }
+        #endregion
+    }
+}
---
+++ Test.CUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static Task1.Class1;
+
+
+namespace Test.CUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task1:");
+            int[] arr = { 1, 2, 3, 4, 3, 2, 1 };
+            Console.WriteLine(ReturnIndex(arr));
+
+            int[] arr1 = { 1, 100, 50, -51, 1, 1 };
+            Console.WriteLine(ReturnIndex(arr1));
+
+            int[] arr2 = { 1, 100, 50, 5, 1, 1 };
+            Console.WriteLine(ReturnIndex(arr2));
+            Console.ReadKey();
+        }
+    }
+}
