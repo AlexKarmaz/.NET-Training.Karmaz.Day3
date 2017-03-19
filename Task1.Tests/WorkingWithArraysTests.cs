6cc9e979c98bfea06618e94352f08611ccafc8bf
AlexKarmaz <karmazct@mail.ru>
2017-03-19 20:16:21 +0300

    Add NUnit tests for Task1 and Task2

---
+++ Task1.Tests/WorkingWithArraysTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class WorkingWithArraysTests
+    {
+        [TestCase(new int[] { 1, 2, 3, 4, 3, 2, 1 }, ExpectedResult = 3)]
+        [TestCase(new int[] { 1, 100, 50, -51, 1, 1 }, ExpectedResult = 1)]
+        [Test]
+        public int ReturnIndex_PositiveTest(int[] array)
+        {
+            return WorkingWithArrays.ReturnIndex(array);
+        }
+
+        [TestCase(new int[] { 1, 100, 50, 51, 1, 1 }, ExpectedResult = -1)]
+        [TestCase(new int[] { 1, -100, 50, 51, 1, 1 }, ExpectedResult = -1)]
+        [Test]
+        public int ReturnIndex_NegativeTest(int[] array)
+        {
+            return WorkingWithArrays.ReturnIndex(array);
+        }
+
+        [TestCase(null)]
+        [Test]
+        public void ReturnIndex_IfArgumentIsNull_ThrowsArgumentNullException(int[] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => WorkingWithArrays.ReturnIndex(array));
+        }
+
+        [TestCase(new int[] {})]
+        [Test]
+        public void ReturnIndex_IfArrayLengthIsZeroOrMoreThan1000_ThrowsException(int[] array)
+        {
+            Assert.Throws<Exception>(() => WorkingWithArrays.ReturnIndex(array));
+        }
+    }
+}
---
+++ Task2.Tests/WorkingWithStringTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using System;
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class WorkingWithStringTests
+    {
+        [TestCase("xyaabbbccccdefww","xxxxyyyyabklmopq", ExpectedResult = "abcdefklmopqwxy")]
+        [TestCase("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz", ExpectedResult = "abcdefghijklmnopqrstuvwxyz")]
+        [Test]
+        public string Longest_PositiveTest(string str1, string str2)
+        {
+            return WorkingWithString.Longest(str1,str2);
+        }
+
+        [TestCase(null,null)]
+        [TestCase("xyaabbbccccdefww", null)]
+        [TestCase(null, "xyaabbbccccdefww")]
+        [Test]
+        public void Longest_IfArgumentsAreNull_ThrowsArgumentNullException(string str1, string str2)
+        {
+            Assert.Throws<ArgumentNullException>(() => WorkingWithString.Longest(str1, str2));
+        }
+    }
+}
